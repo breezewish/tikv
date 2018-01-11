cda03724f27878c4741de3e20d5f0afac3f8add9
Breezewish <me@breeswish.org>
2018-01-11 16:02:37 +0800

    remove locks in snapshot async_work

--- src/grpcworker/task/util.rs
+++ src/grpcworker/task/util.rs
@@ -13,7 +13,7 @@
 
 use storage;
 use kvproto::kvrpcpb;
-use std::{fmt, sync};
+use std::{cell, fmt, sync};
 
 use super::{Error, Step, StepCallback, StepResult, Value, WorkerThreadContext};
 
@@ -26,33 +26,52 @@ pub trait SnapshotStep: Send + fmt::Display {
     fn get_request_context(&self) -> &kvrpcpb::Context;
 }
 
+/// An cell that only ensures safety when its content is taken only once. Its reference
+/// can be shared between multiple threads.
+struct UnsafeOnetimeCell<T> {
+    val: cell::Cell<Option<T>>,
+}
+
+impl<T> UnsafeOnetimeCell<T> {
+    fn new(v: T) -> UnsafeOnetimeCell<T> {
+        UnsafeOnetimeCell {
+            val: cell::Cell::new(Some(v)),
+        }
+    }
+    /// Should be only called once.
+    fn take_once(&self) -> T {
+        self.val.take().unwrap()
+    }
+}
+
+unsafe impl<T> Sync for UnsafeOnetimeCell<T> {}
+
 impl<R: SnapshotStep> Step for R {
     #[inline]
     fn async_work(self: Box<Self>, context: &mut WorkerThreadContext, on_done: StepCallback) {
-        // TODO: replace sync::Arc::new(sync::Mutex::new(..)) with a flagged sync / send struct
-        // to improve performance.
-        //        let on_done = sync::Arc::new(sync::Mutex::new(Some(on_done)));
-        //        let on_done_for_result = on_done.clone();
-        //        let on_done_for_callback = on_done.clone();
+        let on_done = sync::Arc::new(UnsafeOnetimeCell::new(on_done));
+        let on_done_for_result = on_done.clone();
+        let on_done_for_callback = on_done.clone();
         let next_step_builder = self.create_next_step_builder();
         let result = context.engine.async_snapshot(
             self.get_request_context(),
-            box move |(_, snapshot_result)| {
-                //                let mut on_done = on_done_for_callback.lock().unwrap();
-                //                let on_done = on_done.take().unwrap();
-                match snapshot_result {
-                    Ok(snapshot) => {
-                        let next_step = next_step_builder.build(snapshot);
-                        on_done(StepResult::Continue(Box::from(next_step)));
-                    }
-                    Err(e) => {
-                        on_done(StepResult::Finish(
-                            Err(Error::Storage(storage::Error::from(e))),
-                        ));
-                    }
+            box move |(_, snapshot_result)| match snapshot_result {
+                Ok(snapshot) => {
+                    let next_step = next_step_builder.build(snapshot);
+                    (on_done_for_callback.take_once())(StepResult::Continue(Box::from(next_step)));
+                }
+                Err(e) => {
+                    (on_done_for_callback.take_once())(StepResult::Finish(
+                        Err(Error::Storage(storage::Error::from(e))),
+                    ));
                 }
             },
         );
-        result.unwrap();
+        // TODO: Test whether this actually works
+        if let Err(e) = result {
+            (on_done_for_result.take_once())(StepResult::Finish(
+                Err(Error::Storage(storage::Error::from(e))),
+            ));
+        }
     }
 }
